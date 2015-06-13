38d9fe2bcf3442125b41c136ccab6af963af4206
ByteHamster <ByteHamster@github.com>
2015-06-13 12:39:48 +0200

    questions

--- templates/site.js
+++ templates/site.js
@@ -1,4 +1,10 @@
 function selectOS(os) {
     $("#question_os").hide(500);
     $("#question_client").show(500);
+    $("#question_answers > #os").value = os;
+}
+function selectClient(client) {
+    $("#question_client").hide(500);
+    $("#question_level").show(500);
+    $("#question_answers > #client").value = client;
 }
