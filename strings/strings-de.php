45c9a214568cd1d04cd08528e88d513551fc88a2
root <anton.schwarberg@outlook.com>
2015-06-13 13:58:57 +0200

    zwischenspeicherung

--- strings/strings-de.php
+++ strings/strings-de.php
@@ -4,7 +4,7 @@
 $translations = array();
 $translations["LOREM_IPSUM"]    = "Blah blah... Text... Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
 
-$translations["COPYRIGHT"]     = "(c): Jugend Hackt Süd";
+$translations["COPYRIGHT"]     = "©: Jugend Hackt Süd";
 $translations["PAGE_TITLE"]     = "PGP-HowTo";
 $translations["QUESTIONS_TITLE"]     = "Fragen";
 $translations["QUESTIONS_DESCRIPTION"]     = "Um für Sie eine personalisierte Anleitung erstellen zu können, brauchen wir ein paar Informationen zu Ihrem System und der Zeit die Sie zur Verfügung haben.";
