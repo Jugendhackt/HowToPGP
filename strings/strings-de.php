eb483a5cb33c9b3f89b15c09f7e83022aec67b0b
L3b3rwurst <fabian.allgaier99@gmail.com>
2015-06-13 10:39:37 +0200

    Stand 10.40 Uhr - Beginn Startpage Unformatiert

--- strings/strings-de.php
+++ strings/strings-de.php
@@ -5,5 +5,5 @@ $translations = array();
 $translations["LOREM_IPSUM"]    = "Blah blah... Text... Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
 
 $translations["PAGE_TITLE"]     = "PGP-HowTo";
-
-?>
+$translations["SECTION_1"]      = "Haben Sie schonmals von PGP-Verschlüsselung gehört? Ja? Nein?";
+$translations["SECTION_2"]      = "Auf dieser Seite wollen wir Ihnen";
