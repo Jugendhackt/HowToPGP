af6a3ee18b3a5d3a04fdbb535105b342c7fa8f0e
ByteHamster <info@bytehamster.com>
2015-10-10 14:52:02 +0200

    whitespace cleanup

--- snippet-loader.php
+++ snippet-loader.php
@@ -3,36 +3,32 @@
  * $os, $client, $level, $browser
  * 
  */
-if($client =="thunderbird")
-{
-	if($os == "windows"||$os =="osx") {
-		add("W_Thunderbird_install.html");
-	}
-	else if ($os == "linux") {
-		add("L_Thunderbird_install.html");
-	}
+if($client == "thunderbird") {
+    if($os == "windows" || $os == "osx") {
+        add("W_Thunderbird_install.html");
+    } else if ($os == "linux") {
+        add("L_Thunderbird_install.html");
+    }
+} else if($client == "browser") {
+    add("Mailvelope_introduction.html");
+    add("Mailvelope_install.html");
 }
-else if($client == "browser")
-{
-	add("Mailvelope_introduction.html");
-	add("Mailvelope_install.html");
-}
-if($os =="windows"){
+        
+if($os == "windows") {
     add("W_GPG4Win.html");
-}
-else if($os =="osx"){
+} else if($os == "osx") {
     add("M_GPGTools.html");
 }
 
-if($level =="3"){
+if($level == "3") {
     Add("GnuPG.html");
 }
+
 add("Thunderbird_Enigmail.html");
 
-if(($level == "1")|| ($level == "2")){
+if($level == "1" || $level == "2") {
     add("public_private_key_simple.html");
-}
-else{
+} else {
     add("public_private_key_technical.html");
 }
 
@@ -40,6 +36,7 @@ add("Enigmail_create_keys.html");
 add("keyserver.html");
 add("test_email.html");
 add("Nutze_es_richtig.html");
-if($level=="3"){
+
+if($level == "3") {
     add("web_of_thrust.html");
 }
