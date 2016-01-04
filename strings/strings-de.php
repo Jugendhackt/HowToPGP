6f83a52a6e06bda90224aabfb309f99c65b9ac4a
corvusmo <corvusmo@users.noreply.github.com>
2016-01-04 12:08:18 +0100

    changed german formal to informal, to be consistent

--- strings/strings-de.php
+++ strings/strings-de.php
@@ -6,22 +6,22 @@ $translations = array();
 # General page Strings
 $translations["PAGE_TITLE"]         = "HowToPGP";
 $translations["COPYRIGHT"]          = "© Jugend Hackt Süd";
-$translations["DESCRIPTION"]        = "PGP einfach gemacht! Lesen Sie eine persönlich angepasste Anleitung für PGP.";
+$translations["DESCRIPTION"]        = "PGP einfach gemacht! Eine persönlich angepasste Anleitung für PGP.";
 $translations["QUESTIONS_TITLE"]    = "Fragen";
-$translations["SECTION_1"]          = "Haben Sie schon mal von PGP-Verschlüsselung gehört?";
-$translations["SECTION_2"]          = "Mit dieser interaktiven Anleitung wollen wir Ihnen erklären, wie Sie mit Hilfe von PGP Ihre E-Mails verschlüsseln können. Dadurch schützen Sie Ihre Kommunikation und wirken der Massenüberwachung entgegen. Doch zuvor: eine kleine Fragerunde, um rechnerspezifische Ergebnisse liefern zu können.";
+$translations["SECTION_1"]          = "Hast du schon mal von PGP-Verschlüsselung gehört?";
+$translations["SECTION_2"]          = "Mit dieser interaktiven Anleitung wollen wir dir erklären, wie du mit Hilfe von PGP deine E-Mails verschlüsseln kannst. Dadurch schützt du deine Kommunikation und wirkst der Massenüberwachung entgegen. Doch zuvor: eine kleine Fragerunde, um rechnerspezifische Ergebnisse liefern zu können.";
 $translations["CONTINUE"]           = "Fortfahren";
 $translations["SOURCES"]            = "Quellen";
 $translations["BUGREPORT"]          = "Feedback senden";
 
 # Question
-$translations["QUESTIONS_DESCRIPTION"] = "Um für Sie eine personalisierte Anleitung erstellen zu können, brauchen wir ein paar Informationen zu Ihrem System und der Zeit die Sie zur Verfügung haben.";
-$translations["QUESTION_ONE"]       = "Welches Betriebssystem verwenden Sie?";
+$translations["QUESTIONS_DESCRIPTION"] = "Um eine für dich personalisierte Anleitung erstellen zu können, brauchen wir ein paar Informationen zu deinem System und der Zeit, die du zur Verfügung hast.";
+$translations["QUESTION_ONE"]       = "Welches Betriebssystem verwendest du?";
 $translations["LINUX"]              = "Linux";
 $translations["WINDOWS"]            = "Windows";
 $translations["MACOS"]              = "OS X";
 
-$translations["QUESTION_TWO"]       = "Welches E-Mail-Programm verwenden Sie?";
+$translations["QUESTION_TWO"]       = "Welches E-Mail-Programm verwendest du?";
 $translations["THUNDERBIRD"]        = "Thunderbird";
 $translations["INTERNETBROWSER"]    = "Kein Mailprogramm";
     
@@ -29,7 +29,7 @@ $translations["QUESTION_THREE"]     = "Wie Ausführlich?";
 $translations["SLIDER_VALUE_1"]     = "Ich stehe mit Computern auf Kriegsfuß.";
 $translations["SLIDER_VALUE_2"]     = "Schnell zum Ziel";
 $translations["SLIDER_VALUE_3"]     = "Technisches Verständnis";
-$translations["CONTACT_US"]         = "Wenn ihr uns erreichen wollt, schreibt uns doch eine Mail. Unsere PGP-Keys findet ihr hier.";
+$translations["CONTACT_US"]         = "Wenn du uns erreichen willst, schreibt uns doch eine Mail. Unsere PGP-Keys findest du hier.";
 $translations["START_DESCRIPTION"]  = "Anleitung starten!";
 $translations["ERROR404"]           = "Error 404 - Diese Seite existiert nicht!";
 
